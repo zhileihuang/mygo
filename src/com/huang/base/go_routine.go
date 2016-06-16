f86b49cb247b746fad3236d82db9e21ec8c1e7c2
黄志磊 <huangzl@shinemo.com>
2016-06-16 23:54:56 +0800

    go routine pingpong

--- src/com/huang/base/go_routine.go
+++ src/com/huang/base/go_routine.go
@@ -1,12 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	c := make(chan bool)
-	go func() {
-		fmt.Println("Go,Go,Go!!!")
-		c <- true
-	}()
-	<-c
+	//// for test1
+	//c := make(chan bool)
+	//go func() {
+	//	fmt.Println("Go,Go,Go!!!")
+	//	c <- true
+	//	close(c)
+	//}()
+	//for v := range c {
+	//	fmt.Println(v)
+	//}
+
+
+	//for test2
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	c := make(chan bool, 10)
+	for i := 0; i < 10; i++ {
+		go Go(c, i)
+	}
+	for i := 0; i < 10; i++ {
+		<-c
+	}
+}
+
+func Go(c chan bool, index int) {
+
+	a := 1
+	for i := 0; i < 100000000; i++ {
+		a += i
+	}
+
+	fmt.Println(index, a)
+	c <- true
 }
