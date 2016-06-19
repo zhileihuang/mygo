b6ea14fa834183d2a421a117526cdced65f7b7e5
黄志磊 <huangzl@shinemo.com>
2016-06-19 23:20:11 +0800

    init

--- src/com/huang/ch01/cmd.go
+++ src/com/huang/ch01/cmd.go
@@ -1,4 +1,4 @@
-package ch01
+package main
 
 import "flag"
 import "fmt"
@@ -14,11 +14,16 @@ type Cmd struct {
 
 func parseCmd() *Cmd {
 	cmd := &Cmd{}
-	flag.Usage() = printUsage
-
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print helo message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.Parse()
+	return cmd
 }
 
 func printUsage() {
 	fmt.Printf("Usage:%s [-options] class [args...]\n", os.Args[0])
-
 }
